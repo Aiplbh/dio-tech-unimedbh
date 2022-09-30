d3b845297d52a172ddb24bea9cb973f681e23381
Argemiro Leite <aipleite@gmail.com>
2022-09-30 00:58:25 -0300

    String_multiplasLinhas.py file added

---
+++ python-projects/Strings_multiplasLinhas.py
@@ -0,0 +1,20 @@
+# Bootcamp DIO Geração Tech Unimed-BH - Ciência de Dados
+# Modulo: Python para Ciência de Dados - Instrutor: Guilherme Carvalho
+# Aplicação da aula sobre Strings de multiplas linhas
+# Este recurso é muito útil para se definir pequenos Menus em modo CLI
+
+menu = """
+========= MENU ========
+
+[1] ...... Sacar
+[2] ...... Depósito
+[3] ...... Extrato
+
+[0] ...... Sair
+=======================
+"""
+
+print (menu)
+opcao = input ("Entre com sua opção: ")
+
+
